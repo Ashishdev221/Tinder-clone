a40e268de8abeb9e7ab1a7e380d68c0467708330
Ashishdev221 <84578874+Ashishdev221@users.noreply.github.com>
2022-08-06 02:45:40 +0530

    card done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
+import SwipeButtons from "./SwipeButtons";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     <div className="app">
-      
       <Header />
+      <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 
 function TinderCards() {
+  const [people, setPeople] = useState([
+    {
+      name: "Elon Musk",
+      url: "https://m.media-amazon.com/images/M/MV5BMTI5NDY5NjU3NF5BMl5BanBnXkFtZTcwMzQ0MTMyMw@@._V1_UY1200_CR94,0,630,1200_AL_.jpg"
+    },
+    {
+      name: "Jeff Bezos",
+      url: "https://www.biography.com/.image/t_share/MTY2NzA3ODE3OTgwMzcyMjYw/jeff-bezos-andrew-harrer_bloomberg-via-getty-images.jpg",
+    }
+  ]);
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing:" + nameToDelete);
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + "left the screen!");
+  };
   return (
     <div className='TinderCards'>
-        
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
+            onSwipe={(dir) => swiped(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          >
+            <div style={{backgroundImage: `url(${person.url})`}} className="card">
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+
     </div>
   )
 }
-
+//url: "https://m.media-amazon.com/images/M/MV5BMTI5NDY5NjU3NF5BMl5BanBnXkFtZTcwMzQ0MTMyMw@@._V1_UY1200_CR94,0,630,1200_AL_.jpg"
 export default TinderCards;
